030411a55639534ab8416b614ee709aaf06ceb49
vivek <vivekmaurya3m9@gmail.com>
2021-03-28 20:56:23 +0530

    NavBar Completed

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header.jsx'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import Signup from './components/Signup.jsx'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={Signup} />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ Client/src/components/Signup.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Signup() {
+    return (
+        <div>
+            <h1>Outrage World</h1>
+            <a href="http://localhost:5000/auth/google"> Sign up with The Google</a>
+        </div>
+    )
+}
