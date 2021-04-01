11908f4dd4b3caac4ede06a952c67bdceb18771b
vivek <vivekmaurya3m9@gmail.com>
2021-04-01 12:18:13 +0530

    Done with O-Auth

--- Client/src/components/Profile.jsx
+++ Client/src/components/Profile.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 function Profile(props) {
-    // if (!props.user) {
-    //     props.history.push('/')
-    // }
+    if (!props.user) {
+        props.history.push('/')
+    }
     return (
         <>
-            <h1>You Google ID is</h1>
+            <h1>You Google ID is  - {props.user.googleId}</h1>
+            <h1>Your Name is - {props.user.username}</h1>
+            Your Profile is
+            <img src={props.user.picture} alt="UserName" />
         </>
     );
 }
@@ -16,7 +19,9 @@ function Profile(props) {
 
 const mapStateToProps = (state) => {
     console.log("STATE PROFILE", state)
-    return {}
+    return {
+        user: state.auth
+    }
 }
 
 export default connect(mapStateToProps)(Profile)
